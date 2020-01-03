5d024e81b304f901a9150fbeb7e873521210ba0e
jhg6636 <gusrbwjd1229@gmail.com>
2020-01-03 15:17:42 +0900

    Based on App1

---
+++ app/src/main/java/com/example/application2/Fragment1.java
@@ -0,0 +1,164 @@
+package com.example.application2;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.Manifest;
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Point;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.util.SparseBooleanArray;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fragment1 extends Fragment {
+    final List<String> LIST_MENU = MainActivity.names;
+    List<PhoneBook> REF_MENU = MainActivity.phoneBooks;
+    ListViewAdapter adapter;
+    String[] permission_list = { Manifest.permission.WRITE_CONTACTS };
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragment1, null);
+        checkPermission();
+
+        adapter = new ListViewAdapter(getActivity(), R.layout.listview_btn_item, REF_MENU);
+
+        final ListView listview = (ListView) view.findViewById(R.id.listview1);
+        ImageButton add_button = (ImageButton) view.findViewById(R.id.add_btn);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = getLayoutInflater();
+                View view = inflater.inflate(R.layout.phone_add, null);
+                ad.setView(view);
+                ad.setTitle("연락처 추가");       // 제목 설정
+                ad.setMessage("이름과 전화번호를 입력해주세요");   // 내용 설정
+                // EditText 삽입하기
+
+                final Button submit = (Button) view.findViewById(R.id.buttonSubmit);
+                final EditText name = (EditText) view.findViewById(R.id.edittext_name);
+                final EditText phone_num = (EditText) view.findViewById(R.id.edittext_phone);
+                name.setHint("이름을 입력하세요");
+                phone_num.setHint("번호를 입력하세요");
+                final AlertDialog dialog = ad.create();
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        PhoneBook add_phone = new PhoneBook();
+                        // Text 값 받아서 로그 남기기
+                        String full_name = name.getText().toString();
+                        String phone_number = phone_num.getText().toString();
+                        add_phone.setName(full_name);
+                        add_phone.setTel(phone_number);
+
+                        contactAdd(full_name, phone_number);
+                        adapter.phoneBooks.add(add_phone);
+                        dialog.dismiss();     //닫기
+                    }
+                });
+                ad.setNegativeButton("취소하기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();     //닫기
+                    }
+                });
+                dialog.show();
+            }
+        });
+        adapter.notifyDataSetChanged();
+        listview.setAdapter(adapter);
+        REF_MENU = adapter.phoneBooks;
+
+        return view;
+    }
+    public void contactAdd(final String name, final String phone_num){
+        new Thread(){
+            @Override
+            public void run() {
+                ArrayList<ContentProviderOperation> list = new ArrayList<>();
+                try{
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                                    .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                                    .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                                    .build()
+                    );
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)   //이름
+                                    .build()
+                    );
+                    list.add(
+                            ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                                    .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone_num)           //전화번호
+                                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE  , ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)   //번호타입(Type_Mobile : 모바일)
+                                    .build()
+                    );
+                    checkPermission();
+                    getActivity().getContentResolver().applyBatch(ContactsContract.AUTHORITY, list);  //주소록추가
+                    list.clear();   //리스트 초기화
+                }catch(RemoteException e){
+                    e.printStackTrace();
+                }catch(OperationApplicationException e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    public void checkPermission() {
+        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return;
+
+        for (String permission : permission_list) {
+            //권한 허용 여부를 확인한다.
+            int chk = getActivity().checkCallingOrSelfPermission(permission);
+
+            if (chk == PackageManager.PERMISSION_DENIED) {
+                //권한 허용을여부를 확인하는 창을 띄운다
+                requestPermissions(permission_list, 0);
+            }
+        }
+    }
+}
